29038f1a60f9bff1813643f5ca9be722876b33eb
LinEmmy <emmy24@thi.com.tw>
2026-01-28 17:30:34 +0800

    修改大小設定

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,6 @@ export const environment = {
   hmr: false,
 
   // 測試區
-  // serverIP: 'http://220.130.185.37/1493/TFA',
-  // authIP: 'http://220.130.185.37/dev_auth2',
-  // googleAPIKey: 'AIzaSyC1Vss9QPHmf7m3C9Iv9ZOaLxt6YcdpoC0',
-
-   // 測試區
   // serverIP: 'http://220.130.185.37/1572/TFA',
   // authIP: 'http://220.130.185.37/dev_auth2',
   // googleAPIKey: 'AIzaSyC1Vss9QPHmf7m3C9Iv9ZOaLxt6YcdpoC0',
